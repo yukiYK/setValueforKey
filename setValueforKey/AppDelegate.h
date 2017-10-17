ac91aa262611835555a9e71e6479c8a663c7867e
YKyuki <ssss4444222@163.com>
2017-10-17 17:49:08 +0800

    Initial Commit

---
+++ setValueforKey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  setValueforKey
+//
+//  Created by APiX on 2017/10/17.
+//  Copyright © 2017年 APiX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ setValueforKey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  setValueforKey
+//
+//  Created by APiX on 2017/10/17.
+//  Copyright © 2017年 APiX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
